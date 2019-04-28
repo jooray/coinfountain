febb5ce4c2bf05a84a04cff2eeae263b46701eb5
Juraj Bednar <juraj@bednar.io>
2019-04-28 22:26:39 +0200

    upgrade solidity

--- coinfountain.sol
+++ coinfountain.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.5.0;
 contract CoinFountain {
     uint payoutPerPeriod;
     uint periodDuration;
     uint firstTouch;
-    address[] touchers;
+    address[]  touchers;
     address owner;
     
     // fountain information
@@ -13,11 +13,13 @@ contract CoinFountain {
     event FountainFunded(address funder, uint amount);
     event PeriodPaidOut(address toucher, uint periodStart);
     event Touched(address toucher);
+    
+    mapping (address => uint) pendingWithdrawals;
 
     
-    function CoinFountain(uint _payoutPerPeriod, uint _periodDuration, uint _latitude,
-        uint _longtitude, string _description) {
-        firstTouch = now;
+    constructor(uint _payoutPerPeriod, uint _periodDuration, uint _latitude,
+        uint _longtitude, string memory _description) public {
+        firstTouch = block.timestamp;
         payoutPerPeriod = _payoutPerPeriod;
         periodDuration = _periodDuration;
         owner = msg.sender;
@@ -25,45 +27,53 @@ contract CoinFountain {
         latitude = _latitude; longtitude = _longtitude; description = _description;
     }
     
-    function getFountainInformation() constant returns(uint, uint, uint, uint, string, uint, uint) {
+    function getFountainInformation() view public returns(uint, uint, uint, uint, string memory, uint, uint) {
         return (payoutPerPeriod, periodDuration, latitude, longtitude, description,
           firstTouch, touchers.length);
     }    
     
-    function fund() payable {
-        assert(msg.value > 0);
+    function fund() public payable {
+        require(msg.value > 0);
         // yay, we got funded!
-        FountainFunded(msg.sender, msg.value);
+        emit FountainFunded(msg.sender, msg.value);
     }
     
-    function payOut() {
-        if ((firstTouch + periodDuration > now) || (this.balance < payoutPerPeriod) || (touchers.length == 0))
+    function payOut() public {
+        if ((firstTouch + periodDuration > block.timestamp) || (address(this).balance < payoutPerPeriod) || (touchers.length == 0))
             return;
         uint payoutPerToucher = payoutPerPeriod / touchers.length; // should use SafeMath
         for (uint i = 0; i < touchers.length; i++) {
-            touchers[i].transfer(payoutPerToucher);
-            PeriodPaidOut(touchers[i], firstTouch);
+            pendingWithdrawals[touchers[i]] += payoutPerToucher;
+            emit PeriodPaidOut(touchers[i], firstTouch);
         }
         delete touchers;
     }
     
+    function withdraw() public {
+        uint amount = pendingWithdrawals[msg.sender];
+        // Remember to zero the pending refund before
+        // sending to prevent re-entrancy attacks
+        pendingWithdrawals[msg.sender] = 0;
+        msg.sender.transfer(amount);
+    }
+    
     modifier onlyOwner() {
-        assert(msg.sender == owner);
+        require(msg.sender == owner);
         _; // call rest of the function
     }
     
     // called by the fountain hardware to add toucher
-    function touch(address toucher) onlyOwner {
+    function touch(address toucher) onlyOwner public {
         payOut(); // if this is a new period, make sure the last one is settled
         if (touchers.length == 0)
-            firstTouch = now;
+            firstTouch = block.timestamp;
         touchers.push(toucher);
-        Touched(toucher);
+        emit Touched(toucher);
     }
     
     // utility functions
     
-    function changeOwner(address newOwner) onlyOwner {
+    function changeOwner(address newOwner) onlyOwner public {
         owner = newOwner;
     }
 }
